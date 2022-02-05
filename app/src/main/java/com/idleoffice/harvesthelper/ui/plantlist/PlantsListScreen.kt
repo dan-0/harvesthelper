70c0e8d80d22a573e334855e17658e5c33418955
Dan Lowe <dan@danlowe.me>
2022-02-05 03:17:51 -0500

    image display optimizations

--- app/src/main/java/com/idleoffice/harvesthelper/ui/plantlist/PlantsListScreen.kt
+++ app/src/main/java/com/idleoffice/harvesthelper/ui/plantlist/PlantsListScreen.kt
@@ -1,7 +1,7 @@
 package com.idleoffice.harvesthelper.ui.plantlist
 
-import android.net.Uri
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -11,6 +11,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -54,15 +55,19 @@ private fun PlantListItem(it: Plant) {
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             if (it.image != null) {
-                val uri = Uri.parse(
-                    "android.resource://com.idleoffice.harvesthelper/raw/${it.image.substringBeforeLast(".")}"
-                )
-                Image(
-                    painter = rememberImagePainter(uri),
-                    contentDescription = it.description,
-                    contentScale = ContentScale.Crop,
-                    modifier = Modifier.height(200.dp)
-                )
+                val uri = "android.resource://com.idleoffice.harvesthelper/raw/${it.image.substringBeforeLast(".")}"
+                Box(
+                    modifier = Modifier.background(Color.Gray).height(200.dp).fillMaxWidth(),
+                    contentAlignment = Alignment.Center
+                ) {
+
+                    Image(
+                        painter = rememberImagePainter(uri),
+                        contentDescription = it.description,
+                        contentScale = ContentScale.FillHeight,
+                        modifier = Modifier.fillMaxHeight()
+                    )
+                }
             }
             Text(text = it.name)
         }
