aaf72bafb5f935817536ab3453f9efc236ff484f
Dan Lowe <dan@danlowe.me>
2022-08-28 14:38:49 -0400

    fix: singleton  injection for database

--- app/src/main/java/com/idleoffice/harvesthelper/di/modules/database/DatabaseModule.kt
+++ app/src/main/java/com/idleoffice/harvesthelper/di/modules/database/DatabaseModule.kt
@@ -7,12 +7,14 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 class DatabaseModule {
 
     @Provides
+    @Singleton
     fun provideAppDatabase(applicationContext: Application): AppDatabase {
         return Room.databaseBuilder(
             applicationContext,
